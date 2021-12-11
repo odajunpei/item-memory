a1721928f916ea0bcd98df67cc20de77c445aa9e
EC2 Default User <ec2-user@ip-172-31-56-233.ec2.internal>
2021-12-11 11:00:23 +0000

    member

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_104752) do
+ActiveRecord::Schema.define(version: 2021_12_11_105455) do
+
+  create_table "members", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "name_kana"
+    t.integer "telephone_number"
+    t.string "profile_image_id"
+    t.string "introduction"
+    t.string "wanted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_members_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
