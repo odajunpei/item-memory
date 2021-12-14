5401010e7b24422bff4706e57d10c704f10e0654
odajunpei <i.penju1022@gmail.com>
2021-12-14 15:38:27 +0000

    model

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,9 @@
 class Destination < ApplicationRecord
+  belongs_to :member
+
+    validates :postal_code, :address, :name, presence: true
+
+  def destinations_address
+    '〒' + self.postal_code.insert(3, "-") + " " + self.address + " " + self.name
+  end
 end
