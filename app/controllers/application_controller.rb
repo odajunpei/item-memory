0a2ad893986f496157933457dfc8986281cbe094
odajunpei <i.penju1022@gmail.com>
2021-12-14 15:53:09 +0000

    aa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  $tax_rate = 1.08 #税率のグローバル変数
+  $potage = 800 #送料のグローバル変数
+
+  #ログアウト時のパス
+  def after_sign_out_path_for(resource)
+      root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  resources :posts
-  resources :products
+  resources :products do
+    resources :posts
+  end
   resources :inquiries, only: [:new, :create], param: :name
   get 'inquiries/:name/:id' => 'inquiries#show'
+
 end
 
